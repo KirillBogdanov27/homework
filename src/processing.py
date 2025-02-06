3e81156b520e29f6e98dc5e9e1e129cd777953a6
Кирилл Богданов <kirillwork31@mail.ru>
2025-02-07 05:33:40 +0700

    add sort_by_date functional

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,12 @@
-def filter_by_state(list_of_state:list, state='EXECUTED') -> list:
+def filter_by_state(list_of_state: list, state="EXECUTED") -> list:
+    """Возвращает список по состоянию"""
     new_list = []
     for key in list_of_state:
-        if key['state'] == state:
+        if key["state"] == state:
             new_list.append(key)
     return new_list
 
 
-
-
+def sort_by_date(list_of_date: list, descending: bool = True) -> list:
+    """Функция возвращает список на убывание по дате"""
+    return sorted(list_of_date, key=lambda x: x.get("date"), reverse=descending)
